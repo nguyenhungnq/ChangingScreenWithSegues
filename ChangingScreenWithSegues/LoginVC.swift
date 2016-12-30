5c28109fad74eed2d91a88448a1af9ab5e35f0b9
nguyenhungnq <NguyenHung@antonio.local>
2016-12-30 10:50:00 +0700

    2 Screen

---
+++ ChangingScreenWithSegues/LoginVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ChangingScreenWithSegues
+//
+//  Created by NguyenHung on 12/30/16.
+//  Copyright © 2016 NguyenHung. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
+
---
+++ ChangingScreenWithSegues/MusicListVC.swift
@@ -0,0 +1,32 @@
+//
+//  MusicListVC.swift
+//  ChangingScreenWithSegues
+//
+//  Created by NguyenHung on 12/30/16.
+//  Copyright © 2016 NguyenHung. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.blue
+    }
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
