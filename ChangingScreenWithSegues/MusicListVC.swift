6f0c036ee318229732183c0ec33be245b0d77f07
nguyenhungnq <NguyenHung@antonio.local>
2017-01-04 09:25:21 +0700

    cm

--- ChangingScreenWithSegues/MusicListVC.swift
+++ ChangingScreenWithSegues/MusicListVC.swift
@@ -14,6 +14,7 @@ class MusicListVC: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = UIColor.blue
+        //Thay mau background
     }
 
     @IBAction func backBtnPressed(_ sender: AnyObject) {
